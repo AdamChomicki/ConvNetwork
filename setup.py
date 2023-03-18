7ed9eefa8e6b9303dfdbc8133afe3b966a259fc0
AdamChomicki <65819740+AdamChomicki@users.noreply.github.com>
2023-03-18 14:13:48 +0100

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setuptools.setup(
+    name='ConvNetwork',
+    version='22.0.4',
+    author='AdamChomicki',
+    author_email='ad.chomicki@gmail.com',
+    description='Testing installation of Package',
+    url='https://github.com/AdamChomicki/ConvNetwork.git',
+    license='MIT',
+    packages=['ConvNetwork']
+)
