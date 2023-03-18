680ff1535116ff71ed6de6571dd11b91961ba40f
AdamChomicki <65819740+AdamChomicki@users.noreply.github.com>
2023-03-18 16:24:21 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     author='AdamChomicki',
     author_email='ad.chomicki@gmail.com',
     # Needed to actually package something
-    packages=['convnetwork'],
+    src=['convnetwork'],
     # Needed for dependencies
     install_requires= reqs,
     # strongly suggested for sharing
